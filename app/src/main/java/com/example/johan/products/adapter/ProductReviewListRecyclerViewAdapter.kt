1de3e428564947b8e7939766a58fc81f2ce4447c
Johan Alamo <johan.alamo@gmail.com>
2020-03-23 23:02:05 -0300

    add dataBinding to reviews

--- app/src/main/java/com/example/johan/products/adapter/ProductReviewListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/johan/products/adapter/ProductReviewListRecyclerViewAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.johan.products.adapter
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.johan.products.R
+import com.example.johan.products.databinding.LayoutProductReviewListViewHolderBinding
 import com.example.johan.products.response.Review
 import kotlinx.android.synthetic.main.layout_product_review_list_view_holder.view.*
 
@@ -17,30 +19,20 @@ class ProductReviewListRecyclerViewAdapter(
     RecyclerView.Adapter<ProductReviewListRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(
-                R.layout.layout_product_review_list_view_holder,
-                parent,
-                false
-            ) as View
-        return ViewHolder(view)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<LayoutProductReviewListViewHolderBinding>(layoutInflater,
+            R.layout.layout_product_review_list_view_holder, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.view.txtNumReview.text = (position + 1).toString()
-        holder.view.txtReviewStars.text = data[position].rating.toString()
-        holder.view.rtbarProductReview.rating = data[position].rating!!
-
-        holder.view.txtReviewDate.text = data[position].submissionTime?.subSequence(0, 10)
-        holder.view.txtReviewTitle.text = data[position].title
-        holder.view.txtReviewDescription.text = data[position].reviewText
-
-        holder.view.txtReviewNickname.text =
-            context.getString(R.string.commentAuthor, data[position].userNickname)
+        val review = data[position]
+        holder.binding.review = review
+        holder.binding.position = (position + 1)
     }
 
     override fun getItemCount() = data.size
 
     //internal objects
-    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    class ViewHolder(val binding: LayoutProductReviewListViewHolderBinding): RecyclerView.ViewHolder(binding.root)
 }
